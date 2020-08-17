352e35819b2e017c4afc2794932c9c33855fa443
Hampus Lavin <hampus.lavin@gmail.com>
2020-08-17 19:50:08 +0200

    Add tests for equal, min, max

--- tests/rules.spec.js
+++ tests/rules.spec.js
@@ -1,4 +1,4 @@
-import { between } from '../src/rules/index';
+import { between, equal, min, max } from '../src/rules/index';
 
 import cases from 'jest-in-case';
 
@@ -17,3 +17,39 @@ describe('between', () => {
     ]
   );
 });
+
+cases(
+  'equal(value, [comparative])',
+  ({ value, comparative, isValid }) => {
+    expect(equal(value, [comparative])).toBe(isValid);
+  },
+  [
+    { isValid: true, value: 123, comparative: 123 },
+    { isValid: true, value: 'abc', comparative: 'abc' },
+    { isValid: false, value: 33, comparative: 11 }
+  ]
+);
+
+cases(
+  'min(value, [min])',
+  ({ value, minVal, isValid }) => {
+    expect(min(value, [minVal])).toBe(isValid);
+  },
+  [
+    { isValid: true, value: 3, minVal: 1 },
+    { isValid: true, value: 1, minVal: 1 },
+    { isValid: false, value: 0, minVal: 1 }
+  ]
+);
+
+cases(
+  'max(value, [min])',
+  ({ value, maxVal, isValid }) => {
+    expect(max(value, [maxVal])).toBe(isValid);
+  },
+  [
+    { isValid: true, value: 3, maxVal: 3 },
+    { isValid: true, value: 1, maxVal: 3 },
+    { isValid: false, value: 5, maxVal: 3 }
+  ]
+);
