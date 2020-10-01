e1c3ad2e80f974a589f6c13c958f822363f057b8
Alexandre CANTIN <alexandrecantin@MAC459.home>
2020-10-01 20:50:17 +0200

    Use OWASP email REGEX + add tests

--- tests/form.spec.js
+++ tests/form.spec.js
@@ -1,7 +1,9 @@
 import { url as validateUrl } from "../src/rules/url";
+import { email as validateEmail } from "../src/rules/email";
 
 describe("svelte-forms", () => {
   describe("validators", () => {
+
     describe("url", () => {
       it("passes well-formed URLs", () => {
         const goodUrls = [
@@ -13,6 +15,7 @@ describe("svelte-forms", () => {
           expect(validateUrl(url)).toEqual(true);
         });
       });
+
       it("fails malformed URLs", () => {
         const badUrls = ["just_text", "http://", "//a", ":// should fail"];
         badUrls.forEach(url => {
@@ -21,4 +24,24 @@ describe("svelte-forms", () => {
       });
     });
   });
+
+  describe("email", () => {
+
+    it("passes well-formed emails", () => {
+      const goodEmails = ["email@email.com", "my.email@example.fr"];
+      goodEmails.forEach(email => {
+        expect(validateEmail(email)).toEqual(true);
+      });
+    });
+
+    it("fails malformed emails", () => {
+      const badEmails = ["just_text", ".email@email", "email@email@email", "email.@email", "email@email", "email@", "@gmail.com", 12];
+      badEmails.forEach(email => {
+        expect(validateEmail(email)).toEqual(false);
+      });
+    });
+
+  });
+
+
 });
