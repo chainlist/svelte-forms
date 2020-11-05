30da0dc0bb8c79e35a7d7dd6254fd7cee869124e
garlandcrow <garlandcrow00@gmail.com>
2020-11-06 08:55:33 +0900

    add reset fn to store obj

--- src/index.js
+++ src/index.js
@@ -117,7 +117,7 @@ export function bindClass(
 
 export function form(fn, config = {}) {
   const fields = fn.call();
-  const initialFieldsData = Object.fromEntries(
+  let initialFieldsData = Object.fromEntries(
     Object.keys(fields).map((key) => [
       key,
       { name: fields[key].name || key, value: fields[key].value }
@@ -157,6 +157,17 @@ export function form(fn, config = {}) {
     update,
 
     validate() {
+      walkThroughFields(fn, storeValue, initialFieldsData, config);
+    },
+
+    reset() {
+      const fields = fn.call();
+      initialFieldsData = Object.fromEntries(
+        Object.keys(fields).map((key) => [
+          key,
+          { name: fields[key].name || key, value: fields[key].value }
+        ])
+      );
       walkThroughFields(fn, storeValue, initialFieldsData, config);
     }
   };
